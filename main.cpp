e1bd039f9732f071b765064975746b0750adbaad
liem <25520992@gm.uit.edu.vn>
2026-04-29 19:52:46 +0700

    Logic: Xu ly xoa dong va tang diem so

--- main.cpp
+++ main.cpp
@@ -143,17 +143,20 @@ bool canMove(int dx, int dy){
             }
     return true;
 }
-void removeLine(){
-    int j;
-    for (int i = H-2; i >0 ; i-- ){
-        for (j = 0; j < W-1 ; j++)
-            if (board[i][j] == ' ') break;
-        if (j == W-1){
-            for (int ii = i; ii >0 ; ii-- )
-                for (int j = 0; j < W-1 ; j++ ) board[ii][j] = board[ii-1][j];
+void removeLine() {
+    for (int i = H - 2; i > 0; i--) {
+        bool full = true;
+        for (int j = 1; j < W - 1; j++) {
+            if (board[i][j] == ' ') { full = false; break; }
+        }
+        if (full) {
+            score += 10;
+            for (int ii = i; ii > 1; ii--) {
+                for (int j = 1; j < W - 1; j++) board[ii][j] = board[ii - 1][j];
+            }
             i++;
             draw();
-            _sleep(200);
+            Sleep(200);
         }
     }
 }
